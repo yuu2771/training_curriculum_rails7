38cbdbd6dd0589772e77964a4bce4bbc8a286526
yuu2771 <141595992+yuu2771@users.noreply.github.com>
2023-10-04 09:40:24 +0900

    Issue6の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,7 +36,7 @@ class CalendarsController < ApplicationController
       end
 
       wday_num = Date.today.wday
-      if wday_num >= 7  
+      if wday_num + x >= 7  
         wday_num = wday_num -7
       end
 
